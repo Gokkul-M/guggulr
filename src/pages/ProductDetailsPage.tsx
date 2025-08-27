a5b580a426ef8a0a6cd93b71926ac6d66cb649a2
Gokkul-M <gokkulgokkul57@gmail.com>
2025-08-28 00:34:19 +0530

    check-1

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -14,13 +14,13 @@ import blackRaisinsWithSeeds from '../assets/products/Black Raisins with Seed.pn
 import brokenCashews from '../assets/products/Broken Cashews.png';
 import cakeSeeds from '../assets/products/Cake Seeds.png';
 import cherry from '../assets/products/Cherry.png';
-import chiaSeed from '../assets/products/Chia Seed.png'; // fixed variable name
+import chiaSeed from '../assets/products/Chia seed.png'; // fixed variable name
 import driedAmla from '../assets/products/Dried Amla.png';
 import dryDates from '../assets/products/Dry Dates.png';
 import fennelSeeds from '../assets/products/Fennel Seeds.png';
 import fenugreekSeeds from '../assets/products/Fenugreek Seeds.png';
 import fig from '../assets/products/Fig.png';
-import foxtailMillet from '../assets/products/Foxtail Millet.png';
+import foxtailMillet from '../assets/products/foxtail millet.png';
 import greenRaisinsType1 from '../assets/products/Green Raisins (Type 1).png';
 import kasuriMethi from '../assets/products/Kasuri Methi.png';
 import kodoMillet from '../assets/products/Kodo Millet.png';
